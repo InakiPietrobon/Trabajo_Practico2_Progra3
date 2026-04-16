f0386135aa3fb326ab05b61e8746365d2cd75fe6
Needfentanyl <theconquer123@gmail.com>
2026-04-16 14:32:04 -0300

    eeee

--- trabajo_Practico2_Progra3/src/module-info.java
+++ trabajo_Practico2_Progra3/src/module-info.java
@@ -6,4 +6,5 @@
  */
 module trabajo_Practico2_Progra3 {
 	requires java.desktop;
+	requires JMapViewer;
 }
--- trabajo_Practico2_Progra3/src/trabajo_Practico2_Progra3/conectandoLocalidades.java
+++ trabajo_Practico2_Progra3/src/trabajo_Practico2_Progra3/conectandoLocalidades.java
@@ -1,12 +1,22 @@
 package trabajo_Practico2_Progra3;
 
+import java.awt.Color;
+import java.awt.Event;
 import java.awt.EventQueue;
+import java.awt.Point;
 
 import javax.swing.JFrame;
+
+import org.openstreetmap.gui.jmapviewer.Coordinate;
+import org.openstreetmap.gui.jmapviewer.JMapViewer;
+import org.openstreetmap.gui.jmapviewer.MapMarkerDot;
+import org.openstreetmap.gui.jmapviewer.Style;
+
 
 public class conectandoLocalidades {
 
 	private JFrame frame;
+	private JMapViewer mapa;
 
 	/**
 	 * Launch the application.
@@ -38,6 +48,24 @@ public class conectandoLocalidades {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Conectando Localidades");
+		
+		mapa = new JMapViewer();
+		
+		frame.getContentPane().add(mapa);
+		
+		
+		Coordinate coordenada = new Coordinate(-30, -58); 
+		mapa.setDisplayPosition(coordenada, 2);		
+		MapMarkerDot marcador = new MapMarkerDot("Punto 1", coordenada);
+		
+		marcador.getStyle().setBackColor(Color.red);
+		
+		mapa.addMapMarker(marcador);
+		
+		
+			
+		
 	}
 
 }
