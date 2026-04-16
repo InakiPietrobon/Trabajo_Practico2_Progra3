6e267059d78860c65f079b1ff0b6eb0fba0b11a3
Iñaki Pietrobon <theconquer123@gmail.com>
2026-04-16 11:23:17 -0300

    Add files via upload

---
+++ trabajo_Practico2_Progra3/src/trabajo_Practico2_Progra3/conectandoLocalidades.java
@@ -0,0 +1,43 @@
+package trabajo_Practico2_Progra3;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class conectandoLocalidades {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					conectandoLocalidades window = new conectandoLocalidades();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public conectandoLocalidades() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
